48d8016d517395f42aa6e0ed9eb9b4934988cf53
DeveloperAnonymous <40847862+DeveloperAnonymous@users.noreply.github.com>
2021-10-12 18:09:56 -0400

    Revert "Release V2.2"

--- run.py
+++ run.py
@@ -31,11 +31,10 @@ class AdeptClient(commands.Bot):
         if (message.created_at.timestamp() - user.last_message_timestamp) < 0.5:
             user.strikes += 1
             
-            if user.strikes % 3:
+            if user.strikes >= 3:
                 if member.get_role(configs.ADMIN_ROLE) is None and member.get_role(configs.TRUST_ROLE) is None:
-                    times = user.strikes / 3
-                    await tasks.create_mute_task(member, 60 * times)
-                    await message.channel.send(f"{member.mention} est maintenant muet en raison du spam!")
+                    await util.mute(member)
+                    await message.channel.send(f"Nous avons rendu {member.mention} muet en raison du spam!")
                 user.strikes = 0
         else:
             user.strikes = 0
