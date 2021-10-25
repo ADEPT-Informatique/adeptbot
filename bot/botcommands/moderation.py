86411d6f682028c3b55d83b1a7bec3fdc4d65667
DeveloperAnonymous <matthew_quebec@outlook.fr>
2021-10-25 15:27:13 -0400

    #19

--- bot/botcommands/moderation.py
+++ bot/botcommands/moderation.py
@@ -1,6 +1,8 @@
 import discord
 import traceback
 import typing
+
+import re
 from discord.ext import commands
 from discord.ext.commands.context import Context
 from discord.ext.commands.errors import CommandInvokeError
@@ -17,6 +19,13 @@ class ParsedTime:
     def __init__(self, label, seconds) -> None:
         self.label = label
         self.seconds = seconds
+
+
+def parse_values(value: str):
+    pattern = r"(\d+)([A-Za-z]+)"
+    matches = re.match(pattern, value)
+
+    return matches.groups(None)
 
 
 def parse_secs(value: int):
@@ -54,21 +63,24 @@ def parse_week(value: int):
 
 
 class CustomTime(commands.Converter):
-    async def convert(self, _, value) -> ParsedTime:
-        time_parse = value[-1].lower()
-        
-        try:
-            int_value = int(value)
-        except ValueError:
-            int_value = int(value[:-1])
+    async def convert(self, _, value: str) -> ParsedTime:
+        matches = parse_values(value)
 
-        return {
+        if matches is None:
+            raise commands.BadArgument(f"{value} n'est pas une unité de temps valide.")
+
+        int_value = matches[0]
+        time_parse = matches[1]
+
+        parsed_time = {
             's': parse_secs(int_value),
             'm': parse_mins(int_value),
             'h': parse_hours(int_value),
             'd': parse_days(int_value),
             'w': parse_week(int_value)
-        }.get(time_parse, parse_secs(int_value))
+        }.get(time_parse, None)
+        
+        return parsed_time
 
 
 class ModerationCog(commands.Cog):
