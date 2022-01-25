95b53e8784b7da0816bda8af2388ffb8b617a5a8
DeveloperAnonymous <matthew_quebec@outlook.fr>
2022-01-25 00:25:53 -0500

    Small fixes

--- start.sh
+++ start.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-container_id=$(docker container ls -q --filter name=adeptbot)
+# Stop the container
+docker stop adeptbot
 
-# If container_id is empty, then the container is not running
-if [ -z "$container_id" ]; then
-    echo "Container is not running"
-else
-    # Stop the container
-    docker container stop $container_id
-
-    # if logs/ folder doesn't exist, create it
-    if [ ! -d "logs" ]; then
-        mkdir logs
-    fi
-
-    # Save the logs with today's date up to seconds precision
-    nohup docker container logs $container_id >> "logs/$(date +%Y-%m-%d_%H-%M-%S).txt"
-
-    # Delete the container
-    docker container rm $container_id
+# if logs/ folder doesn't exist, create it
+if [ ! -d "logs" ]; then
+    mkdir logs
 fi
+
+# Save the logs with today's date up to seconds precision
+nohup docker logs adeptbot >> "logs/$(date +%Y-%m-%d_%H-%M-%S).txt"
+
+# Delete the container
+docker rm adeptbot
 
 # Rebuild the image and start the container
 docker build -t adeptbot:latest .
