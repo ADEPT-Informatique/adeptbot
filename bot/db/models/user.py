9af0249f57e78fe9a2da801ea8743967c15d7166
DeveloperAnonymous <matthew_quebec@outlook.fr>
2022-02-19 01:16:31 -0500

    #41 Modification du setup et optimisations

--- bot/db/models/user.py
+++ bot/db/models/user.py
@@ -3,12 +3,23 @@ from ..services import BaseService, UserService
 
 
 class WelcomeUser(Entity):
-    def __init__(self, discord_id: int, name: str, email: str, is_student: bool, student_id: int = None, program: str = None, **kwargs) -> None:
+    name: str
+    email: str
+    is_student: bool
+    is_teacher: bool
+    is_it_student: bool
+    student_id: int
+    program: str
+
+    def __init__(self, discord_id: int, name: str, email: str, is_student: bool, **kwargs) -> None:
         self.name = name
         self.email = email
         self.is_student = is_student
-        self.student_id = student_id
-        self.program = program
+
+        self.is_teacher = False
+        self.is_it_student = False
+        self.student_id = None
+        self.program = None
 
         super().__init__(discord_id, **kwargs)
 
---
+++ bot/interactions/base.py
@@ -0,0 +1,39 @@
+from types import NoneType
+from disnake import ButtonStyle, Interaction, ui
+
+class YesNoInteraction(ui.View):
+    """
+    Basic yes/no interaction
+
+    Parameters
+    ------------
+    timeout: Optional[:class:`int`]
+        Timeout in seconds, defaults to 60
+    """
+
+    def __init__(self, timeout: int = 60):
+        super().__init__(timeout=timeout)
+        self.value: bool | NoneType = None
+
+    async def start(self):
+        """
+        Start the interaction and wait for a response from the user or timeout
+
+        return: The value of the interaction if the user responded, `None` if the user timed out
+        """
+        await self.wait()
+
+        return self.value
+
+
+    @ui.button(label="Oui", style=ButtonStyle.green)
+    async def yes(self, button: ui.Button, interaction: Interaction):
+        await interaction.response.defer()
+        self.value = True
+        self.stop()
+
+    @ui.button(label="Non", style=ButtonStyle.red)
+    async def no(self, button: ui.Button, interaction: Interaction):
+        await interaction.response.defer()
+        self.value = False
+        self.stop()
